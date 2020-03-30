fb8d7bbf350dfe0a7f6a1e60126bce227b3c55d8
jaydenfont <62161834+jaydenfont@users.noreply.github.com>
2020-03-29 23:34:41 -0400

    Add files via upload

---
+++ gpa_calculator_class.py
@@ -0,0 +1,163 @@
+
+"""
+GPA Calculator (BU Edition)
+Contains 2 sets of methods, one set to act on a user-defined object,
+and another to prompt the user step-by-step to enter the informaton.
+"""
+
+class Calculator:
+    
+    letter_values = {
+        'A': 4.0,
+        'A-': 3.7,
+        'B+': 3.3,
+        'B': 3.0,
+        'B-': 2.7,
+        'C+': 2.3,
+        'C': 2.0,
+        'C-': 1.7,
+        'D': 1.0,
+        'F': 0.0,
+        '0': 0.0 # in case there is no fifth class, this will ensure the calculation is not affected
+    }
+    
+    buID = "U91735366"
+    
+    def __init__(self, g1, c1, g2, c2, g3, c3, g4, c4, g5, c5): # g = letter grade, c = credit for class
+        
+        self.g1 = g1
+        self.c1 = c1
+        self.g2 = g2
+        self.c2 = c2
+        self.g3 = g3
+        self.c3 = c3
+        self.g4 = g4
+        self.c4 = c4
+        self.g5 = g5
+        self.c5 = c5
+        
+        
+    def calculate_button(g1, c1, g2, c2, g3, c3, g4, c4, g5, c5):
+        # used for GUI
+        total_pts = (Calculator.letter_values.get(g1) * c1) + (Calculator.letter_values.get(g2) * c2) + (Calculator.letter_values.get(g3) * c3) + (Calculator.letter_values.get(g4) * c4) + (Calculator.letter_values.get(g5) * c5)
+        total_credits = c1 + c2 + c3 + c4 + c5
+        gpa = total_pts / total_credits
+        return gpa
+    '''
+    Everything below this point is not needed for the GUI to work, they are just old functions from previous versions
+    '''
+    def calculate_prompt():
+
+        total_pts = 0
+        total_credits = 0
+        course_num = int(input('How many classes are you currently taking? '))
+        for n in range(course_num):  #  loops through the number of courses you inputed
+            grade = input('What was your letter grade for class {}?: '.format(n+1))
+            grade = Calculator.letter_values.get(grade)
+            credit = float(input("How many credits is the class?: "))
+            pts = grade * credit  #  calculates honor points from one class
+            total_pts += pts  #  adds previous points to running total
+            total_credits += credit  #  adds inputed credits to running total
+        gpa = total_pts / total_credits  # calculates GPA
+
+        return gpa, total_credits, total_pts
+    
+    def calculate_obj(self): # will work with values passed into object, not done
+        
+        total_pts = 0
+        total_credits = 0
+        # grade * credits
+    
+        total_pts = (Calculator.letter_values.get(self.g1) * self.c1) + (Calculator.letter_values.get(self.g2) * self.c2) + (Calculator.letter_values.get(self.g3) * self.c3) + (Calculator.letter_values.get(self.g4) * self.c4) + (Calculator.letter_values.get(self.g5) * self.c5)
+        
+        total_credits = self.c1 + self.c2 + self.c3 + self.c4 + self.c5  #  adds inputed credits to running total
+        gpa = total_pts / total_credits  # calculates GPA
+        print("Here are your total honor points:" + str(total_pts))
+        print('Here are you total credits: ' + str(total_credits))
+        return gpa, total_credits, total_pts
+    
+    def round_obj(self, gpa, total_credits, total_pts):
+        
+        round_prompt = input('Round to 2 decimals? Y/N: ')
+       
+        if round_prompt == 'Y' or round_prompt == 'y':
+            gpa = roundFunction(gpa, 2) #  rounds to 2 decimals
+            print(gpa)
+            return total_credits, total_pts
+        else:
+            print(gpa)
+            return total_credits, total_pts
+        
+    def roundFunction(gpa, total_credits, total_pts): # parameters from .calc_with_credits()
+        
+        print("Here are your total honor points:" + str(total_pts))
+        print('Here are you total credits: ' + str(total_credits))
+        round_prompt = input('Round to 2 decimals? Y/N: ')
+       
+        if round_prompt == 'Y' or round_prompt == 'y':
+            gpa = roundFunction(gpa, 2) #  rounds to 2 decimals
+            print(gpa)
+            return total_credits, total_pts
+        else:
+            print(gpa)
+            return total_credits, total_pts
+        
+    def cumulative(total_credits, total_pts): #parameters from .round()
+        
+        old_points = 61.2 + 56.0 + 60.1
+        old_credits = 16 + 16 + 17
+        
+        cumulative_prompt = input('Cumulative? (Jay only): Y/N: ')
+        
+        if cumulative_prompt == 'Y' or cumulative_prompt == 'y':
+            
+            isJay = input("Enter BUID: ")
+            
+            if isJay == Calculator.buID:
+                
+                print('Note: must update honor points and credits after each semester.\nSee "Classes" Tab under Student Link')
+                cumm_points = total_pts + old_points
+                cumm_credits = old_credits + total_credits
+                cgpa = cumm_points / cumm_credits
+                round_prompt = input('Round to 2 decimals? Y/N: ')
+                if round_prompt == 'Y' or round_prompt == 'y':
+                    print('Cumulative GPA: {}'.format(roundFunction(cgpa, 2)))  #  rounds to 2 decimals
+                else:
+                    print('Cumulative GPA: {}'.format(cgpa))
+            else:
+                print('Invalid')
+        else:
+            return print('Okay')
+    
+    def cumulative_obj(self, total_credits, total_pts): #parameters from .round()
+        
+        old_points = 61.2 + 56.0 + 60.1
+        old_credits = 16 + 16 + 17
+        
+        cumulative_prompt = input('Cumulative (up to Fall 2019)? (Jay only): Y/N: ')
+        
+        if cumulative_prompt == 'Y' or cumulative_prompt == 'y':
+            
+            isJay = input("Enter BUID: ")
+            
+            if isJay == Calculator.buID:
+                
+                print('Note: must update honor points and credits after each semester.\nSee "Classes" Tab under Student Link')
+                cumm_points = total_pts + old_points
+                cumm_credits = old_credits + total_credits
+                cgpa = cumm_points / cumm_credits
+                round_prompt = input('Round to 2 decimals? Y/N: ')
+                if round_prompt == 'Y' or round_prompt == 'y':
+                    print('Cumulative GPA: {}'.format(roundFunction(cgpa, 2)))  #  rounds to 2 decimals
+                else:
+                    print('Cumulative GPA: {}'.format(cgpa))
+            else:
+                print('Invalid')
+        else:
+            return print('Okay')
+        
+    def exitFromWindow():
+        input('Press enter to exit')
+
+
+    calculate_prompt()
---
+++ gpa_gui.py
@@ -0,0 +1,242 @@
+"""
+GUI Add-On for GPA Calculator
+Some helpful links:
+http://codeinpython.com/tutorials/learn-use-classes-tkinter/
+https://pythonprogramming.net/change-show-new-frame-tkinter/
+BEST ONES:
+https://python-textbok.readthedocs.io/en/1.0/Introduction_to_GUI_Programming.htmlx
+https://www.geeksforgeeks.org/python-gui-tkinter/
+"""
+import tkinter as tk
+from gpa_calculator_class import Calculator as Calc
+
+
+class MainWindow:
+
+    def simplerCalculate(self):
+        """
+        Saves user inputted grades and credits in list,
+        then passes list values through calculate function to calculate GPA
+        """
+
+        grades = [self.g1entry.get(), self.g2entry.get(), self.g3entry.get(), self.g4entry.get(),
+                  self.g5entry.get()]  # fetches and stores each grade
+        credits = [self.c1entry.get(), self.c2entry.get(), self.c3entry.get(), self.c4entry.get(),
+                   self.c5entry.get()]  # fetches and stores each credit
+        # makes sure each class is has both a letter grade and credit hours
+        gradeEntries = 0
+        creditEntries = 0
+        for i in grades:
+            if i != "":
+                gradeEntries += 1
+        for j in credits:
+            if j != "" or j == 0:
+                creditEntries += 1
+        print(gradeEntries)
+        print(creditEntries)
+        equalLength = True
+        if gradeEntries != creditEntries:
+            equalLength = False
+
+        #  iterates through grade entries to capitalize them for matching to dictionary
+        gradeListIndex = 0
+        for i in grades:
+            if len(i) == 0:  # prevents error when searching for blank spaces in dictionary
+                grades[gradeListIndex] = "0"
+            else:
+                grades[gradeListIndex] = i.upper()
+            gradeListIndex += 1
+        print("Grades: {}".format(grades))
+
+        # iterates through credit entries to convert to int
+
+        creditListIndex = 0
+        for i in credits:
+            if len(i) == 0:  # prevents error when converting blank spaces to int
+                credits[creditListIndex] = 0
+            else:
+                credits[creditListIndex] = int(i)
+            creditListIndex += 1
+        print('Credits: {}'.format(credits))
+
+        # Calculates and rounds, checking to make sure the user input something, and that each class is filled completely
+        if equalLength == True:
+            try:
+                gpa = Calc.calculate_button(grades[0], credits[0], grades[1], credits[1], grades[2], credits[2], grades[3],
+                                            credits[3], grades[4], credits[4])  # WORKS!!!
+            except:
+                self.errorLabel = tk.Label(root, text='Error: Must enter at least one class.', width = 40)
+                self.errorLabel.grid(row=9, column=1)
+                print("user failed to enter data for one class")
+        else:
+            self.mismatchLabel = tk.Label(root, text='Error: Must fill both grade and credits for each class.')
+            self.mismatchLabel.grid(row=9, column=1)
+            print("Mismatch between number of grades and credits")
+        x = self.roundVar.get()  # fetches result from rounding checkbox
+        if x == 1:  # box is checked off
+            print("user chose to round")
+            gpa = round(gpa, 2)
+            print("GPA: " + str(gpa))
+        else:
+            print("user did not round")
+            gpa = gpa
+            print("GPA: " + str(gpa))
+
+        # Displays GPA
+
+        result = "Your Semester GPA is: {}".format(str(gpa))
+        self.gpa_label = tk.Label(root, text=result, font="bold", width=40)
+        self.gpa_label.grid(row=9, column=1)
+
+    def longCalculateFunction(self):  # not using this anymore
+
+        # Fetches the user input from each field and checks it
+
+        class1Grade = self.g1entry.get()  # grabs user input from entry field, saves as string
+        if len(class1Grade) == 0:  # checks if space was left blank
+            class1Grade = '0'  # if string is blank, the entry becomes '0', which is a placeholder for the calculator function
+        else:
+            class1Grade = class1Grade.upper()  # if input is a string, it automatically makes it uppercase
+
+        class2Grade = self.g2entry.get()
+        if len(class2Grade) == 0:
+            class2Grade = '0'
+        else:
+            class2Grade = class2Grade.upper()
+
+        class3Grade = self.g3entry.get()
+        if len(class3Grade) == 0:
+            class3Grade = '0'
+        else:
+            class3Grade = class3Grade.upper()
+
+        class4Grade = self.g4entry.get()
+        if len(class4Grade) == 0:
+            class4Grade = '0'
+        else:
+            class4Grade = class4Grade.upper()
+
+        class5Grade = self.g5entry.get()
+        if len(class5Grade) == 0:
+            class5Grade = '0'
+        else:
+            class5Grade = class5Grade.upper()
+
+        class1Credits = self.c1entry.get()  # grabs user input, saves as string
+        if len(class1Credits) == 0:  # checks if space was left blank
+            class1Credits = 0  # if blank, entry becomes int 0, which is a placeholder for the calculator function
+        else:
+            class1Credits = int(
+                class1Credits)  # if not blank, the string is converted to an int, which allows the calculator function to work
+
+        class2Credits = self.c2entry.get()
+        if len(class2Credits) == 0:
+            class2Credits = 0
+        else:
+            class2Credits = int(class2Credits)
+
+        class3Credits = self.c3entry.get()
+        if len(class3Credits) == 0:
+            class3Credits = 0
+        else:
+            class3Credits = int(class3Credits)
+
+        class4Credits = self.c4entry.get()
+        if len(class4Credits) == 0:
+            class4Credits = 0
+        else:
+            class4Credits = int(class4Credits)
+
+        class5Credits = self.c5entry.get()
+        if len(class5Credits) == 0:
+            class5Credits = 0
+        else:
+            class5Credits = int(class5Credits)
+
+        # Calculates and rounds
+
+        gpa = Calc.calculate_button(class1Grade, class1Credits, class2Grade, class2Credits, class3Grade, class3Credits,
+                                    class4Grade, class4Credits, class5Grade, class5Credits)  # WORKS!!!
+        x = self.roundVar.get()  # fetches result from rounding checkbox
+        print(x)
+        if x == 1:
+            gpa = round(gpa, 2)
+        else:
+            gpa = gpa
+
+        # Displays GPA
+        result = "Your Semester GPA is: {}".format(str(gpa))
+        self.gpa_label = tk.Label(root, text=result, font="bold", width=40)
+        self.gpa_label.grid(row=9, column=1)
+
+    def __init__(self, master):
+        self.master = master
+        master.title("GPA Calculator")
+
+        self.title = tk.Label(master, text='Semester GPA Calculator')
+        self.title.grid(column=1)
+
+        self.instructions = tk.Label(master, text='Enter your information below (leave unused spaces blank)')
+        self.instructions.grid(row=1, column=1)
+
+        self.labelg1 = tk.Label(master, text='Grade from Class 1')
+        self.labelg1.grid(row=3, column=0)
+        self.g1entry = tk.Entry(master)
+        self.g1entry.grid(row=3, column=1)
+
+        self.labelc1 = tk.Label(master, text='Credits for Class 1')
+        self.labelc1.grid(row=4, column=0)
+        self.c1entry = tk.Entry(master)
+        self.c1entry.grid(row=4, column=1)
+
+        self.labelg2 = tk.Label(master, text='Grade from Class 2')
+        self.labelg2.grid(row=3, column=2)
+        self.g2entry = tk.Entry(master)
+        self.g2entry.grid(row=3, column=3)
+
+        self.labelc2 = tk.Label(master, text='Credits for Class 2')
+        self.labelc2.grid(row=4, column=2)
+        self.c2entry = tk.Entry(master)
+        self.c2entry.grid(row=4, column=3)
+
+        self.labelg3 = tk.Label(master, text='Grade from Class 3')
+        self.labelg3.grid(row=5, column=0)
+        self.g3entry = tk.Entry(master)
+        self.g3entry.grid(row=5, column=1)
+
+        self.labelc3 = tk.Label(master, text='Credits for Class 3')
+        self.labelc3.grid(row=6, column=0)
+        self.c3entry = tk.Entry(master)
+        self.c3entry.grid(row=6, column=1)
+
+        self.labelg4 = tk.Label(master, text='Grade from Class 4')
+        self.labelg4.grid(row=5, column=2)
+        self.g4entry = tk.Entry(master)
+        self.g4entry.grid(row=5, column=3)
+
+        self.labelc4 = tk.Label(master, text='Credits for Class 4')
+        self.labelc4.grid(row=6, column=2)
+        self.c4entry = tk.Entry(master)
+        self.c4entry.grid(row=6, column=3)
+
+        self.labelg5 = tk.Label(master, text='Grade from Class 5 (if not overloading, leave blank)')
+        self.labelg5.grid(row=7, column=1)
+        self.g5entry = tk.Entry(master)
+        self.g5entry.grid(row=7, column=2)
+
+        self.labelc5 = tk.Label(master, text='Credits for Class 5 (if not overloading, leave blank)')
+        self.labelc5.grid(row=8, column=1)
+        self.c5entry = tk.Entry(master)
+        self.c5entry.grid(row=8, column=2)
+
+        self.roundVar = tk.IntVar()
+        self.roundBox = tk.Checkbutton(master, text='Round to 2 Decimals?', var=self.roundVar)
+        self.roundBox.grid(row=9, column=3)
+        self.calculateButton = tk.Button(master, text='Calculate', width=10, height=3,
+                                         command=lambda: MainWindow.simplerCalculate(self))
+        self.calculateButton.grid(row=9, column=2)
+
+
+root = tk.Tk(className=" Calculator")
+gui = MainWindow(root)
+root.mainloop()
